72c5bf3a0ea98c3ae75de5a42a7344533db48056
Harold Rodriguez <62029812+Haroldarp@users.noreply.github.com>
2021-03-09 18:37:08 -0400

    added chat functionality

--- chat-server.rb
+++ chat-server.rb
@@ -5,6 +5,68 @@ port = 2000
 
 server = TCPServer.new(port)
 
+def chat(socket, params)
+
+    if params.length() == 1
+        message = params[0]
+        broadcast(socket, message)
+    
+    elsif params.length() == 2
+        username = params[0]
+        message = params[1]
+
+        if @users[username] != nil
+            privateMessage(socket, username, message)
+        else
+            socket.puts "INVALID USER"
+    end
+end
+
+def broadcast(socket, message)
+    sender_key = @users.key(socket)
+    @users.each_value do |socket_value|
+        if socket_value != socket
+            socket_value.puts "#{sender_key}: #{message}"
+        end
+    end
+end
+
+def privateMessage(socket, username, message)
+    sender_key = @users.key(socket)
+    socket_receiver = @users[username]
+    socket_receiver.puts "#{sender_key}: #{message}"
+end
+
+def list(socket)
+    sender_key = @users.key(socket)
+    if (@users.length > 1)
+        users_list = @users.select { |key, value| key != sender_key}
+        socket.puts "#{users_list.keys}"
+    else
+        socket.puts "EMPTY"
+    end
+end
+def login (socket, username)
+    if @users[username] == nil
+        @users[username] = socket
+        socket.puts "OK"
+    else
+        socket.puts "TAKEN"
+    end
+end
+
+def logout (socket)
+    search_key = @users.key(socket)
+    delete = @users.delete(search_key)
+
+    if delete == nil
+        socket.puts "ERROR"
+    else
+        socket.puts "OK"
+        socket.close
+    end
+end
+
 loop do
     clientSocket = server.accept
 
@@ -21,13 +83,19 @@ loop do
 
             case key_command
                 when "/ID"
+                    username = commands[1]
+                    login(socket, username)
 
                 when "/CHAT"
+                    params = commands.split("_", 2)
+                    chat(socket, params)
 
                 when "/LIST"
                     list(socket)
 
                 when "/CLOSE"
+                    logout(socket)
+                    break
 
             else
                 socket.puts "INVALID COMMAND"
@@ -36,12 +104,3 @@ loop do
     end
 end
 
-def list(socket)
-    sender_key = @users.key(socket)
-    if (@users.length > 1)
-        users_list = @users.select { |key, value| key != sender_key}
-        socket.puts "#{users_list.keys}"
-    else
-        socket.puts "EMPTY"
-    end
-end
